6d7704c63cf66f6a4c17afe5c3285f73a4ecd85e
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-09-26 23:44:46 +0200

    cmake: use -print-search-dirs to determine compiler root

--- cmake/toolchains/mingw.cmake
+++ cmake/toolchains/mingw.cmake
@@ -8,19 +8,33 @@ if(NOT CMAKE_C_COMPILER)
 	set(CMAKE_C_COMPILER "$ENV{CC}")
 endif()
 
-execute_process(COMMAND "$ENV{CC}" -print-sysroot
-	RESULTS_VARIABLE CC_SYSROOT_RESULT
-	OUTPUT_VARIABLE CC_SYSROOT_OUTPUT)
-string(STRIP "${CC_SYSROOT_OUTPUT}" CC_SYSROOT_OUTPUT)
+execute_process(COMMAND "$ENV{CC}" -print-search-dirs
+	RESULT_VARIABLE CC_SEARCH_DIRS_RESULT
+	OUTPUT_VARIABLE CC_SEARCH_DIRS_OUTPUT)
 
-if(GCC_SYSROOT_RESULT)
-	message(FATAL_ERROR "Could not determine sysroot")
+if(CC_SEARCH_DIRS_RESULT)
+	message(FATAL_ERROR "Could not determine search dirs")
 endif()
 
-set(CMAKE_FIND_ROOT_PATH
-	${CC_SYSROOT_OUTPUT}
-	${CC_SYSROOT_OUTPUT}/mingw
-)
+string(REGEX MATCH ".*libraries: (.*).*" CC_SD_LIBS "${CC_SEARCH_DIRS_OUTPUT}")
+string(STRIP "${CMAKE_MATCH_1}" CC_SEARCH_DIRS)
+string(REPLACE ":" ";" CC_SEARCH_DIRS "${CC_SEARCH_DIRS}")
+
+foreach(CC_SEARCH_DIR ${CC_SEARCH_DIRS})
+	if(CC_SEARCH_DIR MATCHES "=.*")
+		string(REGEX MATCH "=(.*)" CC_LIB "${CC_SEARCH_DIR}")
+		set(CC_SEARCH_DIR "${CMAKE_MATCH_1}")
+	endif()
+	if(IS_DIRECTORY "${CC_SEARCH_DIR}")
+		if(IS_DIRECTORY "${CC_SEARCH_DIR}/../include" OR IS_DIRECTORY "${CC_SEARCH_DIR}/../lib" OR IS_DIRECTORY "${CC_SEARCH_DIR}/../bin")
+			list(APPEND CC_ROOTS "${CC_SEARCH_DIR}/..")
+		else()
+			list(APPEND CC_ROOTS "${CC_SEARCH_DIR}")
+		endif()
+	endif()
+endforeach()
+
+set(CMAKE_FIND_ROOT_PATH ${CC_ROOTS})
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
